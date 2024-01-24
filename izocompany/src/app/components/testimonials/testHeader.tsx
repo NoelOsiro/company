8503b000513875a7b942a5da210502f74721a3de
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-01-24 03:21:41 +0000

    header

--- izocompany/src/app/components/testimonials/testHeader.tsx
+++ izocompany/src/app/components/testimonials/testHeader.tsx
@@ -2,8 +2,8 @@
 const TestHeader = () => {
     return (
         <>
-            <h2 className="text-4xl font-bold text-gray-100 tracking-tight">
-                A smile <br className="hidden sm:block lg:hidden" /> is an inexpensive way to change your looks
+            <h2 className="text-2xl sm:text-4xl font-bold text-gray-100 tracking-tight">
+                A smile <br className="hidden flex sm:block lg:hidden" /> is an inexpensive way to change your looks
             </h2>
             <p className="mt-4 text-gray-300">
                 Goals are just dreams with deadline
--- izocompany/src/app/components/testimonials/testimonials.tsx
+++ izocompany/src/app/components/testimonials/testimonials.tsx
@@ -36,7 +36,7 @@ const Testimonials = () => {
             style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1606938704652-3e588c2c9fd4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80)' }}>
             <div className="absolute bg-gradient-to-r from-gray-900 to-gray-900 opacity-75 inset-0 z-0"></div>
             <div className="min-h-screen flex justify-center">
-                <div className="grid grid-cols-2 gap-4 items-center z-10">
+                <div className="sm:grid grid-cols-2 gap-4 items-center z-10">
                 <div className="max-w-lg text-center sm:text-left">
                     <TestHeader />
                     <SocialLinks />
